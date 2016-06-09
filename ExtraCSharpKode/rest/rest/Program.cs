59e511723e319897beba435865ef609a0fc73cde
blueio22 <marsor354@gmail.com>
2016-06-09 03:25:52 +0200

    Oppdatering, lagd til realtime sync med Socket.io, i tilleg lagd til testfiler for REST kommunikasjon til C# klient [03.35 - 09.06.2016]

---
+++ ExtraCSharpKode/rest/rest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace rest
+{
+    public class Person {
+        public string name;
+        public string text;
+
+        public Person() {
+        }
+    }
+    class Program
+    {
+            
+        static void Main(string[] args)
+        {
+            if (Console.ReadLine() == "send") {
+                Program prog = new Program();
+                Person pers = new Person();
+                pers.name = "Marius Sørenes";
+                pers.text = "this was sent from c#";
+                prog.sendPost(JsonConvert.SerializeObject(pers));                
+            }
+                while (true) { }
+        }
+
+        public void sendPost(string data) {
+            HttpClient client = new HttpClient();
+            StringContent content = new StringContent(data);
+            content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+
+            client.PostAsync("http://localhost:3000/index/sendsite", content);
+            
+        }
+    }
+}
